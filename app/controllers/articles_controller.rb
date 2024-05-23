c88adde5272c86ed50588da1a91bea5a08beb810
hiro <hiro.libe.0930@gmail.com>
2024-05-23 14:39:55 +0900

    annotate, better_errors, binding_of_caller, hamlitを導入

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -41,6 +41,7 @@ class ArticlesController < ApplicationController
   end
 
   private
+
   def article_params
     params.require(:article).permit(:title, :content)
   end
@@ -48,5 +49,4 @@ class ArticlesController < ApplicationController
   def set_article
     @article = Article.find(params[:id])
   end
-
 end
