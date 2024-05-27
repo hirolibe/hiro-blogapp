13df35a5a245f48ddff2156237b93bb3e397d0c6
hiro <hiro.libe.0930@gmail.com>
2024-05-27 14:21:41 +0900

    Rollbackとmigrationファイルの再生成

--- db/migrate/20240515031228_create_articles.rb
+++ db/migrate/20240515031228_create_articles.rb
@@ -1,8 +1,9 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
-      t.string :title # 短い文字列
-      t.text :content # 長い文字列
+      t.references :user, null: false
+      t.string :title, null: false # 短い文字列
+      t.text :content, null: false # 長い文字列
       t.timestamps
     end
   end
--- db/migrate/20240526070426_add_user_id_articles.rb
+++
@@ -1,7 +0,0 @@
-class AddUserIdArticles < ActiveRecord::Migration[6.0]
-  def change
-    # add_column :articles, :user_id, :integer
-    # :テーブル名, :追加したいカラム名, :データの型
-    add_reference :articles, :user
-  end
-end
