88db6e4aba14934f27415b7f06ad766218bdff67
hiro <hiro.libe.0930@gmail.com>
2024-05-30 15:46:12 +0900

    画像の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,8 @@ class User < ApplicationRecord
   has_many :articles, dependent: :destroy
   has_one :profile, dependent: :destroy
 
+  delegate :birthday, :age, :gender, to: :profile, allow_nil: true
+
   # :articlesは複数形
 
   def has_written?(article)
@@ -32,7 +34,34 @@ class User < ApplicationRecord
   end
 
   def display_name
-    self.email.split('@').first
+    # if profile && profile.nickname
+    #   profile.nickname
+    # else
+    #   self.email.split('@').first
+    # end
+
+    # ぼっち演算子
+    profile&.nickname || self.email.split('@').first
+  end
+
+  # def birthday
+  #   profile&.birthday
+  # end
+
+  # def gender
+  #   profile&.gender
+  # end
+
+  def prepare_profile
+    profile || build_profile
+  end
+
+  def avatar_image
+    if profile&.avatar&.attached?
+      profile.avatar
+    else
+      'default-avatar.png'
+    end
   end
 
 end
