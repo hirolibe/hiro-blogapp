a80dcd2dde6d8d8c29bd9d4cae73aad0815a0aeb
hiro <hiro.libe.0930@gmail.com>
2024-06-05 20:04:51 +0900

    本番環境ではdotenvを読み込まないように変更

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,10 @@ module HiroBlogapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    Bundler.require(*Rails.groups)
-    Dotenv::Railtie.Load
+    if Rails.env.development? || Rails.env.test?
+      Bundler.require(*Rails.groups)
+      Dotenv::Railtie.Load
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
