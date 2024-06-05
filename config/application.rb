42e9bf8568d1ae63d61134b43c67ee19ad612b9c
hiro <hiro.libe.0930@gmail.com>
2024-06-05 15:14:16 +0900

    bundlerのバージョンアップ(Herokuでインストールされるバージョンに変更)

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,8 @@ module HiroBlogapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    Dotenv::Railtie.load
+    Bundler.require(*Rails.groups)
+    Dotenv::Railtie.Load
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
