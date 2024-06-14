b4c2952db15b44fba77d15a90874de685b0ee3c0
hiro <hiro.libe.0930@gmail.com>
2024-06-14 14:13:08 +0900

    フォロー機能の実装(UserモデルとRelationshipモデルのリレーション形成)

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,8 @@ class User < ApplicationRecord
   has_many :articles, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :favorite_articles, through: :likes, source: :article
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
   has_one :profile, dependent: :destroy
 
   delegate :birthday, :age, :gender, to: :profile, allow_nil: true
@@ -50,6 +52,9 @@ class User < ApplicationRecord
     profile&.nickname || self.email.split('@').first
   end
 
+  def follow!(user)
+    following_relationships.create!(following_id: user.id)
+  end
   # def birthday
   #   profile&.birthday
   # end
